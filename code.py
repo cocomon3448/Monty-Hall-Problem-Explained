7e44b7f806b83493e75420317b977c3a6191565b
Rammer3448 <73870100+cocomon3448@users.noreply.github.com>
2021-09-25 23:09:40 +0900

    Add files via upload

---
+++ code.py
@@ -0,0 +1,37 @@
+import random
+from os import system
+
+clear = lambda: system('cls')
+cnt_right = 0
+cnt_wrong = 0
+clear()
+def montihall(time):
+    i = 0
+    while i<time:
+        global cnt_right, cnt_wrong
+        lst = ["car", "2", "3"]
+        pick = random.randint(0,2)
+        opn = random.randint(0,2)
+
+        while True:
+            if lst[opn - 1] != lst[pick-1] and lst[opn -1] != "car":
+                break
+            opn = random.randint(0,2)
+
+        lst.remove(lst[opn-1])
+        lst.remove(lst[pick-1])
+
+        if lst[0] == "car":
+            cnt_right+=1
+        else:
+            cnt_wrong+=1
+        i+=1
+        if i % (time//10) == 0:
+            clear()
+            print("Process:", str(i) + "/" + str(time))
+    print("Done!")
+    print("Result:",(cnt_right/(cnt_wrong+cnt_right))*100,"\b%")
+    print("More Info:", "Car =",cnt_right,"Not Car =",cnt_wrong)
+    
+
+montihall(100000) # 1000 time test
